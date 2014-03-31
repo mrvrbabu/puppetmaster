4c1266c3789fd630ae9c2bd382847ebb9537ea1b
Ramesh Babu <mr.vrbabu@hotmail.com>
2014-04-01 00:53:23 +0530

    Adding manifest folder

---
+++ manifests/nodes.pp
@@ -0,0 +1,25 @@
+node 'masterserver.homeoffice.net' {
+	class { 'homeoffice_role_motd' :}
+		}
+
+
+node 'kvmnode6401.homeoffice.net' {
+        class { 'homeoffice_role_motd' :}
+                }
+
+node 'kvmnode6402.homeoffice.net' {
+        class { 'homeoffice_role_motd' :}
+                }
+
+node 'kvmnode6403.homeoffice.net' {
+        class { 'homeoffice_role_motd' :}
+                }
+
+node 'kvmnode6404.homeoffice.net' {
+        class { 'homeoffice_role_motd' :}
+                }
+
+node 'kvmnode6405.homeoffice.net' {
+        class { 'homeoffice_role_motd' :}
+                }
+
