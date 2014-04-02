7e6ce763f7f5bfed3d685c2bbb04c0df4c4c3f05
Ramesh Babu <mr.vrbabu@hotmail.com>
2014-04-02 22:09:06 +0530

    adding homeoffice_role_memcached module to kvmnode6401 host

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,25 +1,26 @@
 node 'masterserver.homeoffice.net' {
-	class { 'homeoffice_role_motd' :}
-		}
+  class { 'homeoffice_role_motd' :}
+  }
 
 
 node 'kvmnode6401.homeoffice.net' {
-        class { 'homeoffice_role_motd' :}
-                }
+  class { 'homeoffice_role_motd' :}
+  class { 'homeoffice_role_memcached' :}
+  }
 
 node 'kvmnode6402.homeoffice.net' {
-        class { 'homeoffice_role_motd' :}
-                }
+  class { 'homeoffice_role_motd' :}
+  }
 
 node 'kvmnode6403.homeoffice.net' {
-        class { 'homeoffice_role_motd' :}
-                }
+  class { 'homeoffice_role_motd' :}
+  }
 
 node 'kvmnode6404.homeoffice.net' {
-        class { 'homeoffice_role_motd' :}
-                }
+  class { 'homeoffice_role_motd' :}
+  }
 
 node 'kvmnode6405.homeoffice.net' {
-        class { 'homeoffice_role_motd' :}
-                }
+  class { 'homeoffice_role_motd' :}
+  }
 
