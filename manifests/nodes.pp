3d262467c92664a2512bcef70b76513925600396
Ramesh Babu <mr.vrbabu@hotmail.com>
2014-04-03 17:26:11 +0530

    Adding virtualhost config for websites cats and dogs

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -11,11 +11,24 @@ node 'kvmnode6401.homeoffice.net' {
 node 'kvmnode6402.homeoffice.net' {
   class { 'homeoffice_role_motd' :}
   class { 'homeoffice_role_httpd' :}
+  $site_name = 'cats'
+  $site_domain_name = 'cats.homeoffice.net'
+  file { '/etc/httpd/conf.d/vhost.conf' :
+  content => template('homeoffice_role_httpd/vhost.conf.erb'),
+  notify  => Service['httpd'],
   }
+  }
+
 
 node 'kvmnode6403.homeoffice.net' {
   class { 'homeoffice_role_motd' :}
   class { 'homeoffice_role_httpd' :}
+  $site_name = 'dogs'
+  $site_domain_name = 'dogs.homeoffice.net'
+  file { '/etc/httpd/conf.d/vhost.conf' :
+  content => template('homeoffice_role_httpd/vhost.conf.erb'),
+  notify  => Service['httpd'],
+  }
   }
 
 node 'kvmnode6404.homeoffice.net' {
