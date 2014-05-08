a76488585b6786d2bd1e705e35fbe6556a3793c1
rbabu <rbabu@homeoffice.net>
2014-05-08 11:48:20 +0530

    Fixing syntax errors

--- modules/tomcatsingleinstance/manifests/repo.pp
+++ modules/tomcatsingleinstance/manifests/repo.pp
@@ -2,7 +2,7 @@ class tomcatsingleinstance::repo {
   yumrepo { 'Master_server':
     descr     => 'Master server repo',
     gpgcheck  => 0,
-    enbled    => 1,
+    enabled    => 1,
     baseurl   => 'ftp://ftp://192.168.2.10/pub/rhel64_64/Server', 
   }
 }
