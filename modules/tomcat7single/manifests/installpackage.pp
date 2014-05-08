ec379f528bce5da170fc06d17cda8ec3fd0e25eb
Ramesh Babu <mr.vrbabu@hotmail.com>
2014-05-08 15:27:15 +0530

    update installpackage.pp

--- modules/tomcat7single/manifests/installpackage.pp
+++ modules/tomcat7single/manifests/installpackage.pp
@@ -1,3 +1,4 @@
+#Install java package
 class tomcat7single::installpackage {
   package { 'java-1.7.0-openjdk':
       ensure => latest,
@@ -6,9 +7,9 @@ class tomcat7single::installpackage {
       ensure => latest,
       require => Package['java-1.7.0-openjdk'],
     }
-    package { 'tomcat7':
-      ensure => installed,
-      require => [Package['java-1.7.0-openjdk'], Yumrepo['jpackage']],
-    }
+  #  package { 'tomcat7':
+  #    ensure => installed,
+  #    require => [Package['java-1.7.0-openjdk'], Yumrepo['jpackage']],
+  #  }
   
 }
