ebaf174aee94e86a29fd7186d2589d3638a78a26
rbabu <rbabu@homeoffice.net>
2014-05-08 15:59:33 +0530

    Updated java -version

--- modules/tomcat7single/manifests/installpackage.pp
+++ modules/tomcat7single/manifests/installpackage.pp
@@ -1,10 +1,10 @@
 #Install java package
 class tomcat7single::installpackage {
   package { 'java-1.7.0-openjdk.x86_64 ':
-      ensure => latest,
+      ensure => installed,
     }
     package { 'java-1.7.0-openjdk-devel.x86_64 ':
-      ensure => latest,
+      ensure => installed,
       require => Package['java-1.7.0-openjdk.x86_64 '],
     }
   #  package { 'tomcat7':
