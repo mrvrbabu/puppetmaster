25e91ceaf7762fb57d743b2230bb4959a21913f0
rbabu <rbabu@homeoffice.net>
2014-05-08 16:08:19 +0530

    java

--- modules/tomcat7single/manifests/repo.pp
+++ modules/tomcat7single/manifests/repo.pp
@@ -3,7 +3,7 @@ class tomcat7single::repo {
     descr    =>    "Master repository",
     baseurl  =>    "ftp://192.168.2.10/pub/rhel64_64/Server",
     enabled  => 1,
-    gpgcheck => 1,
+    gpgcheck => 0,
   }
   
   }
