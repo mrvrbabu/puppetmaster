f022fb2e27130135a55020608d45ae7fc59fad97
root <ramesh.babu3@gmail.com>
2016-04-19 07:06:45 -0400

    modified init.pp file in new_feature

--- modules/helloworld/manifests/init.pp
+++ modules/helloworld/manifests/init.pp
@@ -3,39 +3,6 @@
 #
 # Full description of class helloworld here.
 #
-# Parameters
-# ----------
-#
-# Document parameters here.
-#
-# * `sample parameter`
-# Explanation of what this parameter affects and what it defaults to.
-# e.g. "Specify one or more upstream ntp servers as an array."
-#
-# Variables
-# ----------
-#
-# Here you should define a list of variables that this module would require.
-#
-# * `sample variable`
-#
-# Examples
-# --------
-#
-# @example
-#    class { 'helloworld':
-#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
-#    }
-#
-# Authors
-# -------
-#
-# Author Name <author@domain.com>
-#
-# Copyright
-# ---------
-#
-# Copyright 2016 Your name here, unless otherwise noted.
 #
 #class helloworld {
 #
